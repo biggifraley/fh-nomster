4a296501fbaa7fab3fdffabc74086c6884b7350c
Biggi Fraley <biggi.fraley@usa.net>
2015-10-06 23:39:19 +0000

    Add picture field to model, controller, and upload form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006200430) do
+ActiveRecord::Schema.define(version: 20151006233130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20151006200430) do
     t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "picture"
   end
 
   add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
